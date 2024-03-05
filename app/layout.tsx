2a126fdba550cab8d28fa8b9db2740469923162e
Daniel Gil <dannyjaygil@gmail.com>
2024-03-05 14:40:05 +0200

    Show the issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} light`}>
+      <body className={inter.variable}>
         <Theme accentColor="violet">
           <NavBar />
           <main className="p-5">{children}</main>

554e1dbc90e35b898cb207a93c03f9a8743bfda9
Daniel Gil <dannyjaygil@gmail.com>
2024-03-09 08:51:49 +0200

    Build the assignee filter component

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -4,11 +4,15 @@ import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 import IssueStatusFilter from "./IssueStatusFilter";
+import IssueAssigneeFilter from "./IssueAssigneeFilter";
 
 const IssueActions = () => {
   return (
     <Flex justify="between">
-      <IssueStatusFilter />
+      <Flex gap="3" className="flex-col sm:flex-row">
+        <IssueStatusFilter />
+        <IssueAssigneeFilter />
+      </Flex>
       <Button>
         <Link href="/issues/new">New Issue</Link>
       </Button>
---
+++ app/issues/list/IssueAssigneeFilter.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Status, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import { Skeleton } from "@/app/components";
+
+const IssueAssigneeFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const { data: users, error, isLoading } = useUsers();
+
+  if (isLoading) return <Skeleton height="2rem" />;
+
+  if (error) return null;
+
+  return (
+    <Select.Root
+      defaultValue={searchParams.get("assignee") || ""}
+      onValueChange={(assignee) => {
+        const params = new URLSearchParams();
+        if (assignee !== "ALL") params.append("assignee", assignee);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        const query = params.size ? "?" + params.toString() : "";
+        router.push(`/issues/list${query}`);
+      }}
+    >
+      <Select.Trigger placeholder="Filter by assignee..." />
+      <Select.Content>
+        <Select.Item value={"ALL"}>All</Select.Item>
+        {users?.map((user) => (
+          <Select.Item key={user.id} value={user.id || "ALL"}>
+            {user.name}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
+
+export default IssueAssigneeFilter;
